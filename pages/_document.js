869d7417de09da6f1dcdb3d1c6efccebc45102b0
ChristianWork05 <christian.lanza@recargaexpress.com>
2025-03-12 08:11:24 -0400

    Refactor app structure and update font styles

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
       </Head>
-      <body className="antialiased">
+      <body>
         <Main />
         <NextScript />
       </body>
