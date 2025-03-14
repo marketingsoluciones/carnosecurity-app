f93c76678518a0e7d8c9a1abddbdeb870ce08b3e
ChristianWork05 <christian.lanza@recargaexpress.com>
2025-03-14 11:41:26 -0400

    Agregar logotipo Carbo en formato PNG

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -109,78 +109,68 @@ export default function LandingPage() {
         </div>
       </section>
 
-      <footer className="relative h-screen text-center bg-white">
-        <div className="relative z-10 flex flex-col items-center justify-center h-full w-full p-8">
-          <h2 className="text-3xl mb-4 text-[#004AAD] font-bold"> DEMANDER UN DEVIS</h2>
-          <div className="flex flex-col md:flex-row items-center justify-center w-full">
-            <div className="md:w-1/2 p-4">
-              
-            <div className="mb-4">
-            <p className="mb-4 text-justify">Chez CARBONASECURITY nous réalisons une étude détaillée et entièrement personnalisée des besoins de sécurité spécifiques requis par votre célébration, événement ou situation particulière.</p>
-            <p className="mb-4 text-justify">Si vous le souhaitez, nous préparerons un budget basé sur les besoins de votre entreprise, sans aucun type d'engagement et en moins de 24 heures vous pourrez l'avoir entre vos mains pour l'étudier attentivement.</p>
-            </div>
-              
-              <h3 className="text-2xl mb-4 text-[#004AAD] font-bold">VOUS POUVEZ NOUS CONTACTER AU</h3>
-              <div className="flex flex-col justify-center mb-4">
-                <div className="flex items-center mx-2 text-[#004AAD]">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-                  </svg>
-                  <span>+33 6 08 01 24 87   Info.</span>
-                </div>
-                <div className="flex items-center mx-2 text-[#004AAD]">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                  </svg>
-                  <span>contact@carbosecurity.com</span>
-                </div>
-              </div>
-              <h3 className="text-2xl mb-4 text-[#004AAD] font-bold">OU NOUS VISITER</h3>
-              
-              <div className="flex flex-col justify-start">
-                
-                <div className="flex items-start ">
-                  <span className="text-black font-semibold">Siège social :</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-[#004AAD]">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                  </svg>
-                  <span className="font-semibold text-[#004AAD]">29, place de la République, 72600 Mamers-France</span>
-                </div>
 
-
-                <div className="flex items-start ">
-                  <div className="text-black font-semibold">Ventes et formation:</div>
-                  
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-[#004AAD]">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                  </svg>
-
-                  <div className="font-semibold text-[#004AAD]">173 rue de Charenton 75012 Paris - France</div>
-                </div>
-
-
-              </div>
-
-            </div>
-            <div className="md:w-1/2 p-4">
-            <iframe
-  width="540"
-  height="305"
-  src="https://sibforms.com/serve/MUIFAMAWyL_2Hq5SNuqb3lfEZaDngVPZk3pi2DB82AU6JCxxN4eoMs3-x0dB55ew602EkLZlyqWlmIHIyVq7ogxIkj2W-4aa9M1A8rl8yG4E5bzeTj5I73o1KBlrD2vSPPLoNs5z29MBYPfDZMHcY_0tGRltJEyNaJ2iwBtwHZQrCGUrgO6AIPHokIFW1x70WioY193DlcY4yrn6"
-  frameBorder="0"
-  scrolling="auto"
-  allowFullScreen
-  style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%' }}
-></iframe>
-            </div>
+<footer className="relative text-center bg-white">
+  <div className="relative z-10 flex flex-col items-center justify-center w-full p-8">
+    <h2 className="text-3xl mb-4 text-[#004AAD] font-bold"> DEMANDER UN DEVIS</h2>
+    <div className="flex flex-col md:flex-row items-center justify-center w-full">
+      <div className="md:w-1/2 p-4">
+        <div className="mb-4">
+          <p className="mb-4 text-justify">Chez CARBONASECURITY nous réalisons une étude détaillée et entièrement personnalisée des besoins de sécurité spécifiques requis par votre célébration, événement ou situation particulière.</p>
+          <p className="mb-4 text-justify">Si vous le souhaitez, nous préparerons un budget basé sur les besoins de votre entreprise, sans aucun type d'engagement et en moins de 24 heures vous pourrez l'avoir entre vos mains pour l'étudier attentivement.</p>
+        </div>
+        <h3 className="text-2xl mb-4 text-[#004AAD] font-bold">VOUS POUVEZ NOUS CONTACTER AU</h3>
+        <div className="flex flex-col justify-center mb-4">
+          <div className="flex items-center mx-2 text-[#004AAD]">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+            </svg>
+            <span>+33 6 08 01 24 87 Info.</span>
           </div>
-          <div className="flex flex-col justify-between w-full mt-8">
-            <div className="flex items-center justify-center gap-2 bg-[#345073] p-2 text-white">
-              <p>Mentions légales - Politique de confidentialité - Système de gestion intégré - Vous souhaitez travailler avec nous ?</p>
-              <button className="flex rounded-md bg-green-600 hover:bg-emerald-700 text-white p-2">Envoyez votre CV</button>
-            </div>
+          <div className="flex items-center mx-2 text-[#004AAD]">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            </svg>
+            <span>contact@carbosecurity.com</span>
+          </div>
+        </div>
+        <h3 className="text-2xl mb-4 text-[#004AAD] font-bold">OU NOUS VISITER</h3>
+        <div className="flex flex-col justify-start">
+          <div className="flex items-start">
+            <span className="text-black font-semibold">Siège social :</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-[#004AAD]">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+            </svg>
+            <span className="font-semibold text-[#004AAD]">29, place de la République, 72600 Mamers-France</span>
+          </div>
+          <div className="flex items-start">
+            <div className="text-black font-semibold">Ventes et formation:</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-[#004AAD]">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+            </svg>
+            <div className="font-semibold text-[#004AAD]">173 rue de Charenton 75012 Paris - France</div>
+          </div>
+        </div>
+      </div>
+      <div className="md:w-1/2 p-4">
+        <iframe
+          width="540"
+          height="305"
+          src="https://sibforms.com/serve/MUIFAMAWyL_2Hq5SNuqb3lfEZaDngVPZk3pi2DB82AU6JCxxN4eoMs3-x0dB55ew602EkLZlyqWlmIHIyVq7ogxIkj2W-4aa9M1A8rl8yG4E5bzeTj5I73o1KBlrD2vSPPLoNs5z29MBYPfDZMHcY_0tGRltJEyNaJ2iwBtwHZQrCGUrgO6AIPHokIFW1x70WioY193DlcY4yrn6"
+          frameBorder="0"
+          scrolling="auto"
+          allowFullScreen
+          style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%' }}
+        ></iframe>
+      </div>
+    </div>
+    <div className="flex flex-col justify-between w-full mt-8">
+      <div className="flex items-center justify-center gap-2 bg-[#345073] p-2 text-white">
+        <p>Mentions légales - Politique de confidentialité - Système de gestion intégré - Vous souhaitez travailler avec nous ?</p>
+        <button className="flex rounded-md bg-green-600 hover:bg-emerald-700 text-white p-2">Envoyez votre CV</button>
+      </div>
       <div className="flex items-start justify-start text-[#004AAD]">
         <p>CARBOSECURITY est une marque appartenant à l’entreprise CARBONAPH R&D (N°905146791 RCS Le Mans) et enregistré au près du CNAPS, Conseil national des activités privées de sécurité Établissement public sous tutelle du Ministère de l'Intérieur sous le numéro d’agrément suivant : 069-2024-03-27-2024052409.</p>
       </div>
